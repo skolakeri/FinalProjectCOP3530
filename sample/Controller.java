880f5d4a7b571456e591be66c86618a39d353d62
jessicabogart <57964339+jessicabogart@users.noreply.github.com>
2020-11-30 17:06:55 -0500

    Add files via upload

---
+++ sample/Controller.java
@@ -0,0 +1,24 @@
+package sample;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import javax.swing.*;
+import java.io.IOException;
+
+public class Controller extends Main{
+
+
+    public void handleSelectionClick()  {
+    }
+    public void handleInsertionClick(){
+    }
+    public void handleBubbleClick(){
+    }
+    public void handleTablesClick() {
+    }
+    public void handleTreesClick() {
+    }
+}
